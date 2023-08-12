21ad7230cfce33d8b4504d7241b99681d8b48dfb
Chelsea <chelsea@tripwiretech.com>
2023-08-13 09:49:19 +1200

    Initial Commit

---
+++ ListTutorial/ListTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTutorialApp.swift
+//  ListTutorial
+//
+//  Created by Charles Suddens on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ListTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
